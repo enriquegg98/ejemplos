b52c8ffb1d5bc0b5ae8df026cc8e1f11495fe8ab
enriquegg98 <lgarciagonzalez98@gmail.com>
2024-01-26 12:56:56 -0500

    Se sube un nuevo cambio en el archivo 17

---
+++ ejemplo17.php
@@ -0,0 +1,14 @@
+<! -- Funciones en PHP -- !>
+
+<?php
+
+    function imprimirNombre($nombre,$apellido)
+    {
+        // Rutina de instrucciones
+        echo "Hola ".$nombre." ".$apellido."<br/>";
+    }
+    // Llamar a la función
+    imprimirNombre("Oscar","Uh");
+    imprimirNombre("Pedro","Gonzalez");
+    imprimirNombre("Raul","Martinez");
+?>
